fa3b81d93085810882fcaf4495a9086f5744f2e2
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-12-30 17:11:09 +0300

    ~ initial commit

--- ktor-features/ktor-locations/jvm/src/io/ktor/locations/Location.kt
+++ ktor-features/ktor-locations/jvm/src/io/ktor/locations/Location.kt
@@ -11,6 +11,7 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.util.pipeline.*
 import io.ktor.routing.*
+import kotlinx.serialization.*
 import kotlin.reflect.*
 
 /**
@@ -25,6 +26,7 @@ annotation class KtorExperimentalLocationsAPI
  */
 @KtorExperimentalLocationsAPI
 @Target(AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS)
+@SerialInfo
 annotation class Location(val path: String)
 
 /**
